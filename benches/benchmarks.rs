a31d71f169627980b8cb28075e6d0ea0093ac6e0
Janus <janus@insignificancegalore.net>
2019-02-12 18:57:31 +0100

    Adding benchmarks

---
+++ benches/benchmarks.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+
+extern crate colorguess;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let all = colorguess::build_all_configs();
+
+    c.bench_function("count all", move |b| b.iter(|| colorguess::count_outcomes(&[1,2,3,4], &all)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
